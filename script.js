735f186175184f487fb9a578edf6b61bb3618af3
victoralvessantos02-svg <victor.alves.santos02@escola.pr.gov.br>
2026-09-16 13:37:12 -0300

    456

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir) {
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+function curtir(){
+    const contador = botaoCurtir.querySelector("span");
+    if(curtiu === false){
+        contador.textContent++;
+        curtiu = true;}
+        else{
+            contador.textCOntent--;
+            curtiu = false;
+        }
+}
+});
